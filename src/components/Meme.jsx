d0d576810d6dd19863ae93f7b7e217d0d3c81dcc
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-29 16:27:15 +0530

    Add images to the meme generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,34 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "./Meme.css";
 import memeData from "../memeData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
   function getMemeImage(e) {
     e.preventDefault();
     const memesArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-
-    console.log(url);
+    setMemeImage(`${memesArray[randomNumber].url}`);
   }
 
   return (
-    <form className="form-container">
-      <input
-        type="text"
-        id="top-text"
-        name="top-text"
-        placeholder="Shut up"
-      ></input>
-      <input
-        type="text"
-        id="bottom-text"
-        name="bottom-text"
-        placeholder="and take my money"
-      ></input>
-      <button type="submit" className="submit-btn" onClick={getMemeImage}>
-        Get a new meme image
-      </button>
-    </form>
+    <main>
+      <form className="form-container">
+        <input
+          type="text"
+          id="top-text"
+          name="top-text"
+          placeholder="Shut up"
+        ></input>
+        <input
+          type="text"
+          id="bottom-text"
+          name="bottom-text"
+          placeholder="and take my money"
+        ></input>
+        <button type="submit" className="submit-btn" onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </form>
+      <img src={memeImage}  className="meme-image"/>
+    </main>
   );
 }
