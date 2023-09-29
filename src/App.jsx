9f17e9c35403b1669b151e883ebae435670f3990
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-29 11:13:09 +0530

    import Header to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>This is App component</h1>
+      <Header />
     </>
   );
 }
